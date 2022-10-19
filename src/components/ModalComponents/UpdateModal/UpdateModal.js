2e8070fb79048c013620aae7f418e02296b81287
Chingun <zolboo@Chinguns-iMac-2.local>
2022-10-19 07:15:23 -0700

    did little styling

--- src/components/ModalComponents/UpdateModal/UpdateModal.js
+++ src/components/ModalComponents/UpdateModal/UpdateModal.js
@@ -3,24 +3,27 @@ import "./UpdateModal.css";
 
 const UpdateModal = ({ selectedPost, closeModal }) => {
   return (
-    <div>
-      <div>
+    <div className="updateModalMainContainer">
+      <div className="updateTitleContainer">
         <span>Update User</span>
       </div>
       <div>
-        <div>
+        <div className="updateDataContainer">
+          <label>Title</label>
           <input placeholder="Title" defaultValue={selectedPost?.title} />
+          <label>First Name</label>
           <input
             defaultValue={selectedPost?.firstName}
             placeholder="First Name"
           />
+          <label>Last Name</label>
           <input
             placeholder="Last Name"
             defaultValue={selectedPost?.lastName}
           />
         </div>
       </div>
-      <div>
+      <div className="updateButtonsContainer">
         <button onClick={() => closeModal()}>Cancel</button>
         <button>Update</button>
       </div>
